f1ce7f79532dd676ad63d5253b53bb18ccf2f39e
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-01-05 14:05:54 +0100

    use let instead of instance variable

--- test/formable_test.rb
+++ test/formable_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 describe Shaf::Formable do
-  before do
-    @class = Class.new do
+  let(:clazz) do
+    Class.new do
       include Shaf::Formable
     end
   end
 
   it "adds form class method" do
-    assert @class.respond_to? :form
+    assert clazz.respond_to? :form
   end
 
   it "creates duplicate create and edit forms" do
-    @class.form do
+    clazz.form do
       name 'Foo'
       fields(
         {
@@ -27,43 +27,43 @@ describe Shaf::Formable do
         }
       )
     end
-    assert_instance_of(Shaf::Formable::Form, @class.create_form)
-    assert_instance_of(Shaf::Formable::Form, @class.edit_form)
-    refute_equal @class.edit_form.object_id, @class.create_form.object_id
-    assert_equal 'Foo', @class.create_form.name
-    assert_equal 'Foo', @class.edit_form.name
+    assert_instance_of(Shaf::Formable::Form, clazz.create_form)
+    assert_instance_of(Shaf::Formable::Form, clazz.edit_form)
+    refute_equal clazz.edit_form.object_id, clazz.create_form.object_id
+    assert_equal 'Foo', clazz.create_form.name
+    assert_equal 'Foo', clazz.edit_form.name
   end
 
   it "creates a create form" do
-    @class.form do
+    clazz.form do
       create do
         name 'Create Form'
         title 'create-form'
       end
     end
 
-    assert_nil @class.edit_form
-    assert_instance_of(Shaf::Formable::Form, @class.create_form)
-    assert_equal 'Create Form', @class.create_form.name
-    assert_equal 'create-form', @class.create_form.title
+    assert_nil clazz.edit_form
+    assert_instance_of(Shaf::Formable::Form, clazz.create_form)
+    assert_equal 'Create Form', clazz.create_form.name
+    assert_equal 'create-form', clazz.create_form.title
   end
 
   it "creates a edit form" do
-    @class.form do
+    clazz.form do
       edit do
         name 'Edit Form'
         title 'edit-form'
       end
     end
 
-    assert_nil @class.create_form
-    assert_instance_of(Shaf::Formable::Form, @class.edit_form)
-    assert_equal 'Edit Form', @class.edit_form.name
-    assert_equal 'edit-form', @class.edit_form.title
+    assert_nil clazz.create_form
+    assert_instance_of(Shaf::Formable::Form, clazz.edit_form)
+    assert_equal 'Edit Form', clazz.edit_form.name
+    assert_equal 'edit-form', clazz.edit_form.title
   end
 
   it "creates different create and edit forms" do
-    @class.form do
+    clazz.form do
       name 'Common Name'
       create do
         method :post
@@ -75,23 +75,23 @@ describe Shaf::Formable do
         type :bar
       end
     end
-    assert_equal 'Common Name', @class.create_form.name
-    assert_equal 'Common Name', @class.edit_form.name
-    assert_equal :foo, @class.create_form.type
-    assert_equal :bar, @class.edit_form.type
-    assert_equal 'POST', @class.create_form.method
-    assert_equal 'PATCH', @class.edit_form.method
+    assert_equal 'Common Name', clazz.create_form.name
+    assert_equal 'Common Name', clazz.edit_form.name
+    assert_equal :foo, clazz.create_form.type
+    assert_equal :bar, clazz.edit_form.type
+    assert_equal 'POST', clazz.create_form.method
+    assert_equal 'PATCH', clazz.edit_form.method
   end
 
   it "is possible to get the edit form from instances" do
-    @class.form do
+    clazz.form do
       edit do
         name 'Edit Form'
         title 'edit-form'
       end
     end
 
-    object = @class.new
+    object = clazz.new
     assert_instance_of(Shaf::Formable::Form, object.edit_form)
     assert_equal 'Edit Form', object.edit_form.name
     assert_equal object, object.edit_form.resource
