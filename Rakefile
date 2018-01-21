6ea9a7570c8970bc7a338da589dfe5be02fe2cfd
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-01-21 11:20:15 +0100

    Adding migration tests

--- Rakefile
+++ Rakefile
@@ -2,7 +2,7 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |t|
   t.libs << 'test'
-  t.pattern = 'test/*test.rb'
+  t.pattern = 'test/**/*test.rb'
 end
 
 desc "Run tests"
---
+++ test/generator/migration_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+module Shaf
+  module Generator
+    describe Migration do
+      let(:output_file) { "" }
+      let(:output) { "" }
+
+      let(:write_stub) do
+        lambda do |file, content|
+          output_file << file
+          output << content
+        end
+      end
+
+      before do
+        File.stub :write, write_stub do
+          Dir.stub :exist?, true do
+            migration.call
+          end
+        end
+      end
+
+      describe "create table" do
+        let(:table_name) { "blogs" }
+        let(:migration) do
+          Migration.new(*%w(create table blog message:string user_id:integer))
+        end
+
+        it "creates file in db/migrations" do
+          assert output_file.start_with? "db/migrations"
+        end
+
+        it "adds timestamp to filename" do
+          file = File.basename output_file
+          assert_match /\A\d{14}_/, file
+        end
+
+        it "names the migration file correctly" do
+          file = File.basename output_file
+          assert_match /_create_#{table_name}_table\.rb\Z/, file
+        end
+      end
+
+      describe "add column" do
+        let(:migration) do
+          Migration.new(*%w(add column blogs comment:string))
+        end
+
+        it "creates file in db/migrations" do
+          assert output_file.start_with? "db/migrations"
+        end
+      end
+
+    end
+  end
+end
