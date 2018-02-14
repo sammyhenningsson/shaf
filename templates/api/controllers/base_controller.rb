f5e9bb6ecc674bd749a88fb65180b111c601cc73
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-02-14 16:32:13 +0100

    log exeptions

--- templates/api/controllers/base_controller.rb
+++ templates/api/controllers/base_controller.rb
@@ -32,15 +32,26 @@ class BaseController < Sinatra::Base
     log.debug "Payload: #{payload || 'empty'}"
   end
 
-  error Shaf::Authorize::PolicyViolationError do
-    err = ForbiddenError.new
-    respond_with err, status: err.http_status, serializer: ErrorSerializer
-  end
-
   error StandardError do
     err = env['sinatra.error']
-    err = ServerError.new(err.message) unless err.is_a? ServerError
-    respond_with err, status: err.http_status, serializer: ErrorSerializer
+    log.error err.message
+    err.backtrace.each(&log.method(:error))
+
+    api_error = to_api_error(err)
+
+    respond_with api_error,
+      status: api_error.http_status,
+      serializer: ErrorSerializer
   end
 
+  def to_api_error(err)
+    case err
+    when Shaf::Authorize::PolicyViolationError
+      ForbiddenError.new
+    when ServerError
+      err
+    else
+      ServerError.new(err.message)
+    end
+  end
 end
