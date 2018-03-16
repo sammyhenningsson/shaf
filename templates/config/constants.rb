cb857718f98d92fb4fa1fd212e77a6844ccf4463
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-03-16 09:37:14 +0100

    Require files in src directory

--- templates/config/constants.rb
+++ templates/config/constants.rb
@@ -1,5 +1,6 @@
 APP_ROOT = File.expand_path('../', __dir__)
 APP_DIR = File.expand_path('api', APP_ROOT)
+SRC_DIR = File.expand_path('src', APP_ROOT)
 VIEWS_DIR = File.join(APP_ROOT, Shaf::Settings.views_folder)
 ASSETS_DIR = File.join(APP_ROOT, Shaf::Settings.public_folder)
 PAGINATION_PER_PAGE = Shaf::Settings.paginate_per_page || 25
--- templates/config/directories.rb
+++ templates/config/directories.rb
@@ -21,12 +21,24 @@ def sort_files(files)
   end
 end
 
-Dir.chdir(APP_DIR) do
+def require_ruby_files
   files = Dir[File.join('**', '*.rb')]
   sort_files(files).each do |file|
     # load all files with .rb extension in subfolders of api
     $logger&.debug "Require file: #{file}"
     require file
   end
+end
+
+if Dir.exist? SRC_DIR
+  $:.unshift SRC_DIR
+
+  Dir.chdir(SRC_DIR) do
+    require_ruby_files
+  end
+end
+
+Dir.chdir(APP_DIR) do
+  require_ruby_files
 end
 
