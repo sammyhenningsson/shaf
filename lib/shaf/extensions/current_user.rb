0f3e5d89af7b5bb711f677126f13b045627b3565
Sammy Henningsson <sammy.henningsson@hemnet.se>
2018-03-22 11:35:56 +0100

    Refactored CurrentUser

--- lib/shaf/extensions/current_user.rb
+++ lib/shaf/extensions/current_user.rb
@@ -3,34 +3,30 @@ require 'digest'
 module Shaf
   module CurrentUser
     def self.registered(app)
-      return unless app.respond_to?(:current_user)
+      return unless app.respond_to?(:current_user) && app.current_user
 
-      if app.current_user
-        app.log.info "Using Shaf::CurrentUser"
-        app.helpers Helpers
-      end
+      app.log.info 'Using Shaf::CurrentUser'
+      app.helpers Helpers
     end
 
-    def current_user_lookup(&block)
+    def lookup_user_with(&block)
       unless block_given? && block.respond_to?(:call)
-        raise ArgumentError, "::current_user_lookup requires a block argument"
+        raise ArgumentError, '::lookup_user_with requires a block argument'
       end
-      log.info "Using custom current_user lookup"
-      Helpers.set_lookup_proc(block)
+      log.info 'Using custom current_user lookup'
+      Helpers.lookup_proc = block
     end
   end
 
   module Helpers
     class << self
+      attr_accessor :lookup_proc
+
       def user_lookup(token)
-        return @lookup_proc.call(token) if defined?(@lookup_proc)
+        return lookup_proc.call(token) if lookup_proc
         return unless token
         digest = Digest::SHA256.hexdigest(token)
         User.where(auth_token_digest: digest).first
-      end
-
-      def set_lookup_proc(block)
-        @lookup_proc = block
       end
     end
 
@@ -38,6 +34,10 @@ module Shaf
       return @current_user if defined?(@current_user)
       header = settings.auth_token_header
       @current_user = Helpers.user_lookup(request.env[header])
+    end
+
+    def authenticated?
+      !current_user.nil?
     end
   end
 end
