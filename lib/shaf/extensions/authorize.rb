4785424a5a096ba2675f8d1dcd77d42fde24bbcf
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-04-09 21:28:12 +0200

    policy and method_for in helper should be private

--- lib/shaf/extensions/authorize.rb
+++ lib/shaf/extensions/authorize.rb
@@ -18,12 +18,6 @@ module Shaf
   end
 
   module Helpers
-    def policy(resource)
-      return @policy if defined?(@policy) && @policy
-      user = current_user if respond_to? :current_user
-      @policy = self.class.policy_class&.new(user, resource)
-    end
-
     def authorize(action, resource = nil)
       policy(resource) or raise Authorize::NoPolicyError
       @policy.public_send method_for(action)
@@ -31,6 +25,14 @@ module Shaf
 
     def authorize!(action, resource = nil)
       authorize(action, resource) or raise Authorize::PolicyViolationError
+    end
+
+    private
+
+    def policy(resource)
+      return @policy if defined?(@policy) && @policy
+      user = current_user if respond_to? :current_user
+      @policy = self.class.policy_class&.new(user, resource)
     end
 
     def method_for(action)
