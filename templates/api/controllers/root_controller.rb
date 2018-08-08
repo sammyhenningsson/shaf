eb08d02ae9d02022db133b882a02d265a210e88b
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-08-08 07:33:08 +0200

    Some environment settings

--- templates/api/controllers/root_controller.rb
+++ templates/api/controllers/root_controller.rb
@@ -3,7 +3,7 @@ class RootController < BaseController
   HTTP_CACHE_MAX_AGE = 86400 # 60 * 60 * 24 = 1 day
   register_uri :root, '/'
 
-  get root_uri do
+  get :root_uri do
     cache_control(:private, max_age: HTTP_CACHE_MAX_AGE) if Shaf::Settings.http_cache
     respond_with nil, serializer: RootSerializer
   end
--- templates/config/initializers/hal_presenter.rb
+++ templates/config/initializers/hal_presenter.rb
@@ -1,4 +1,4 @@
-if Sinatra::Application.settings.environment == :development
+if [:development, :test].include? Sinatra::Application.settings.environment
   port = Shaf::Settings.port ? ":#{Shaf::Settings.port}" : ""
   HALPresenter.base_href = "http://localhost#{port}"
 end
