c3a5283af5feee446880ad56c82aab171de8f188
Sammy Henningsson <sammy.henningsson@gmail.com>
2018-08-09 01:37:10 +0200

    Refactor migration initializer

--- templates/config/initializers/db_migrations.rb
+++ templates/config/initializers/db_migrations.rb
@@ -2,17 +2,30 @@ require 'sequel'
 require 'config/database'
 
 Sequel.extension :migration
-return unless Dir[File.join(MIGRATIONS_DIR, "*")].any?
-return if Sequel::Migrator.is_current?(DB, MIGRATIONS_DIR)
+Dir[File.join(MIGRATIONS_DIR, "*")]
 
-environment = ENV['RACK_ENV']
-
-if environment == 'test'
-  $logger.info "Running migrations in 'test' environment.."
-  Sequel::Migrator.run(DB, "#{APP_ROOT}/db/migrations")
-else
-  msg = "Database for environment '#{environment}' is not " \
-    "updated to the latest migration"
-  STDERR.puts msg
-  $logger&.warn msg
+def is_current?
+  return true unless Dir[File.join(MIGRATIONS_DIR, "*")].any?
+  Sequel::Migrator.is_current?(DB, MIGRATIONS_DIR)
 end
+
+def environment
+  ENV['RACK_ENV']
+end
+
+def init
+  return if is_current?
+
+  if environment == 'test'
+    $logger.info "Running migrations in 'test' environment.."
+    Sequel::Migrator.run(DB, "#{APP_ROOT}/db/migrations")
+  else
+    msg = "Database for environment '#{environment}' is not " \
+      "updated to the latest migration"
+    STDERR.puts msg
+    $logger&.warn msg
+  end
+end
+
+init
+
