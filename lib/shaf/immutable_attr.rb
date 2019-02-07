0a17e0ee8aed442b3dff3d81822089cc3a6cb9ce
Sammy Henningsson <sammy.henningsson@gmail.com>
2019-02-07 22:03:53 +0100

    DRY up some code

--- lib/shaf/immutable_attr.rb
+++ lib/shaf/immutable_attr.rb
@@ -34,18 +34,8 @@ module Shaf
     end
 
     def immutable_accessor(*methods)
-      methods.each do |method|
-        define_method(method) do
-          value = instance_variable_get(:"@#{method}")
-          ImmutableAttr.dup(value)
-        end
-        define_method(:"#{method}=") do |value|
-          instance_variable_set(
-            :"@#{method}",
-            ImmutableAttr.dup(value)
-          )
-        end
-      end
+      immutable_reader(*methods)
+      immutable_writer(*methods)
     end
   end
 end
