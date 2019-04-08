b1876bf24be9d426d3686338a6c3ac165c0f09b5
Sammy Henningsson <sammy.henningsson@gmail.com>
2019-04-08 16:15:25 +0200

    Fix: Incorrect Cache-Control Header

--- lib/shaf/helpers/cache_control.rb
+++ lib/shaf/helpers/cache_control.rb
@@ -1,21 +1,20 @@
 module Shaf
   module CacheControl
-
     def cache_control(*args, **kwargs)
       return unless Shaf::Settings.http_cache
-      set_max_age(kwargs)
+      __rewrite_max_age(kwargs)
       super(*args, **kwargs)
     end
 
     private
 
-    def set_max_age(kwargs)
-      max_age = kwargs[:http_cache_max_age] or return
+    def __rewrite_max_age(kwargs)
+      max_age = kwargs.delete(:http_cache_max_age)
       if max_age.is_a? Symbol
         key = "http_cache_max_age_#{max_age}".to_sym
-        max_age = Settings.respond_to?(key) ? Settings.send(key) : 86400
-        kwargs[:http_cache_max_age] = max_age
+        max_age = Settings.respond_to?(key) ? Settings.send(key) : 86_400
       end
+      kwargs[:max_age] ||= max_age
     end
   end
 end
