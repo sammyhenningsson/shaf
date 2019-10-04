14b4aeea6a786d8de295f86993ad73dff4fed1bd
Sammy Henningsson <sammy.henningsson@gmail.com>
2019-10-04 15:49:38 +0200

    Refactor model for documentation

--- lib/shaf/doc_model.rb
+++ lib/shaf/doc_model.rb
@@ -4,9 +4,11 @@ module Shaf
   class DocModel
     class << self
       def find(name)
-        @@docs ||= {}
-        @@docs[name] ||= load(name)
-        new(name) if @@docs[name]
+        unless docs[name]
+          properties = load(name) or return
+          docs[name] = new(name, properties)
+        end
+        docs[name]
       end
 
       def find!(name)
@@ -15,40 +17,54 @@ module Shaf
 
       private
 
+      def docs
+        @docs ||= {}
+      end
+
       def load(name)
         file = File.join(Settings.documents_dir, "#{name}.yml")
         return YAML.load(File.read(file)) if File.exist? file
       end
     end
+    
+    attr_reader :name, :attributes, :links, :curies, :embeds
 
-    def initialize(name)
-      @name = name
+    def initialize(name, **kwargs)
+      @name       = name
+      @attributes = kwargs.fetch(:attributes, {})
+      @links      = kwargs.fetch(:links, {})
+      @curies     = kwargs.fetch(:curies, {})
+      @embeds     = kwargs.fetch(:embeds, {})
     end
 
     def to_s
-      return "#{@name} not found" unless @@docs[@name]
-      JSON.pretty_generate(@@docs[@name])
+      JSON.pretty_generate(
+        attributes: attributes,
+        links: links,
+        curies: curies,
+        embeds: embeds,
+      )
     end
 
     def attribute(attr)
-      attr_doc = @@docs.dig(@name, 'attributes', attr.to_s)
+      attr_doc = attributes[attr.to_s]
       return attr_doc if attr_doc
       raise Errors::NotFoundError,
-        "No documentation for #{@name} attribute '#{attr}'"
+        "No documentation for #{name} attribute '#{attr}'"
     end
 
     def link(rel)
-      link_doc = @@docs.dig(@name, 'links', rel.to_s)
+      link_doc = links[rel.to_s]
       return link_doc if link_doc
       raise Errors::NotFoundError,
-        "No documentation for #{@name} link relation '#{rel}'"
+        "No documentation for #{name} link relation '#{rel}'"
     end
 
     def embedded(name)
-      embed_doc = @@docs.dig(@name, 'embeds', name.to_s)
+      embed_doc = embeds[name.to_s]
       return embed_doc if embed_doc
       raise Errors::NotFoundError,
-        "No documentation for #{@name} embedded '#{name}'"
+        "No documentation for #{name} embedded '#{name}'"
     end
   end
 end
