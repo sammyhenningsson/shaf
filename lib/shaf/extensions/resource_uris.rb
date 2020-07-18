3132c9335d4e19e375413cbcc8c447f7ab2a5777
Sammy Henningsson <sammy.henningsson@gmail.com>
2020-07-19 00:17:31 +0200

    Append _collection to uri helpers instead of arg
    When name and plural_name is the same, we used to generate methods that
    return the resource path or collection path depending on arguments
    passed to the helper method. This complicates the code and is more
    confusing to "share" a helper for both the resource and the collection.

--- lib/shaf/extensions/resource_uris.rb
+++ lib/shaf/extensions/resource_uris.rb
@@ -107,11 +107,15 @@ module Shaf
 
     def call
       if plural_name == name
+        # Deprecated code path
+        # Remove this branch and only keep the `else` behavior when dropping
+        # support for this
         register_resource_helper_by_arg
       else
         register_collection_helper
         register_resource_helper
       end
+
       register_new_resource_helper
       register_edit_resource_helper
       @added_path_methods
@@ -125,7 +129,9 @@ module Shaf
       return if skip? :collection
 
       template_uri = "#{base}/#{plural_name}".freeze
-      register(plural_name, template_uri)
+      method_name = plural_name
+      method_name = "#{name}_collection" if name == @plural_name
+      register(method_name, template_uri)
     end
 
     def register_resource_helper
@@ -140,7 +146,8 @@ module Shaf
     # as argument and the resources uri when no arguments are provided.
     def register_resource_helper_by_arg
       return register_resource_helper if skip? :collection
-      return register_collection_helper if skip? :new
+      register_collection_helper
+      return if skip? :new
 
       resource_template_uri =   "#{base}/#{plural_name}/:id"
       collection_template_uri = "#{base}/#{plural_name}"
@@ -180,24 +187,26 @@ module Shaf
   end
 
   class MethodBuilder
+    NO_GIVEN_VALUE = Object.new
+
     def self.query_string(query)
-      return "" unless query.any?
-      "?#{query.map { |key,value| "#{key}=#{value}" }.join("&")}"
+      return '' unless query.any?
+      "?#{query.map { |key, value| "#{key}=#{value}" }.join('&')}"
     end
 
     def initialize(name, uri, alt_uri: nil)
       @name = name
-      @uri = uri
-      @alt_uri = alt_uri
+      @uri = uri.dup.freeze
+      @alt_uri = alt_uri.dup.freeze
     end
 
     def call
       if UriHelper.respond_to? uri_method_name
         exception = ResourceUris::UriHelperMethodAlreadyExistError
-        raise exception.new(@name, uri_method_name)
+        raise exception.new(name, uri_method_name)
       end
 
-      if @alt_uri.nil?
+      if alt_uri.nil?
         build_methods
       else
         build_methods_with_optional_arg
@@ -205,6 +214,8 @@ module Shaf
     end
 
     private
+
+    attr_reader :name, :uri, :alt_uri
 
     def build_methods
       UriHelperMethods.eval_method uri_method_string
@@ -225,11 +236,11 @@ module Shaf
     end
 
     def uri_method_name
-      "#{@name}_uri".freeze
+      "#{name}_uri".freeze
     end
 
     def path_method_name
-      "#{@name}_path".freeze
+      "#{name}_path".freeze
     end
 
     def path_matcher_name
@@ -244,81 +255,94 @@ module Shaf
       "#{uri_method_name}_template".freeze
     end
 
-    def uri_signature(optional_args: 0)
-      signature(uri_method_name, optional_args: optional_args)
+    def uri_signature(uri: @uri, optional_args: 0)
+      signature(uri_method_name, uri, optional_args: optional_args)
     end
 
-    def path_signature(optional_args: 0)
-      signature(path_method_name, optional_args: optional_args)
+    def path_signature(uri: @uri, optional_args: 0)
+      signature(path_method_name, uri, optional_args: optional_args)
     end
 
-    def signature(method_name, optional_args: 0)
-      s = "#{method_name}("
-
-      symbols = extract_symbols.size.times.map { |i| "arg#{i}" }
+    def signature(method_name, uri, optional_args: 0)
+      symbols = extract_symbols(uri).size.times.map { |i| "arg#{i}" }
       sym_count = symbols.size
 
       args = []
-      symbols.each_with_index do |arg, i|
-        if i < sym_count - optional_args
-          args << "arg#{i}"
-        else
-          args << "arg#{i} = nil"
-        end
-      end
-      s << (args.empty? ? "**query)" : "#{args.join(', ')}, **query)")
+      symbols.each_with_index { |_arg, i| args << "arg#{i}" }
+      optional_args.times { |i| args << "arg#{sym_count + i} = nil" }
+      args << '**query'
+
+      "#{method_name}(#{args.join(', ')})"
     end
 
     def uri_method_string
       base_uri = UriHelper.base_uri
-      <<~Ruby
+      <<~RUBY
         def #{uri_signature}
           query_str = Shaf::MethodBuilder.query_string(query)
-          \"#{base_uri}#{interpolated_uri_string(@uri)}\#{query_str}\".freeze
+          \"#{base_uri}#{interpolated_uri_string(uri)}\#{query_str}\".freeze
         end
-      Ruby
+      RUBY
     end
 
     def path_method_string
-      <<~Ruby
+      <<~RUBY
         def #{path_signature}
           query_str = Shaf::MethodBuilder.query_string(query)
-          \"#{interpolated_uri_string(@uri)}\#{query_str}\".freeze
+          \"#{interpolated_uri_string(uri)}\#{query_str}\".freeze
         end
-      Ruby
+      RUBY
     end
 
     def uri_method_with_optional_arg_string
       base_uri = UriHelper.base_uri
-      arg_no = extract_symbols.size - 1
-      <<~Ruby
-        def #{uri_signature(optional_args: 1)}
+      arg_no = extract_symbols(alt_uri).size
+      <<~RUBY
+        def #{uri_signature(uri: alt_uri, optional_args: 1)}
           query_str = Shaf::MethodBuilder.query_string(query)
           if arg#{arg_no}.nil?
-            \"#{base_uri}#{interpolated_uri_string(@alt_uri)}\#{query_str}\".freeze
+            warn <<~DEPRECATION
+
+              Deprecated use of collection uri helper:
+              To get the collection uri use ##{name}_collection_uri instead of ##{uri_method_name}.
+              Or pass an argument to ##{uri_method_name} to get the uri to a resource.
+              \#{caller.find { |s| !s.match? %r{lib/shaf/extensions/resource_uris.rb} }}
+
+            DEPRECATION
+
+            \"#{base_uri}#{interpolated_uri_string(alt_uri)}\#{query_str}\".freeze
           else
-            \"#{base_uri}#{interpolated_uri_string(@uri)}\#{query_str}\".freeze
+            \"#{base_uri}#{interpolated_uri_string(uri)}\#{query_str}\".freeze
           end
         end
-      Ruby
+      RUBY
     end
 
     def path_method_with_optional_arg_string
-      arg_no = extract_symbols.size - 1
-      <<~Ruby
-        def #{path_signature(optional_args: 1)}
+      arg_no = extract_symbols(alt_uri).size
+      <<~RUBY
+        def #{path_signature(uri: alt_uri, optional_args: 1)}
           query_str = Shaf::MethodBuilder.query_string(query)
           if arg#{arg_no}.nil?
-            \"#{interpolated_uri_string(@alt_uri)}\#{query_str}\".freeze
+            warn <<~DEPRECATION
+
+              Deprecated use of collection path helper:
+              To get the collection path use ##{name}_collection_path instead of ##{path_method_name}.
+              Or pass an argument to ##{path_method_name} to get the path to a resource.
+              \#{caller.find { |s| !s.match? %r{lib/shaf/extensions/resource_uris.rb} }}
+
+            DEPRECATION
+
+            \"#{interpolated_uri_string(alt_uri)}\#{query_str}\".freeze
           else
-            \"#{interpolated_uri_string(@uri)}\#{query_str}\".freeze
+            \"#{interpolated_uri_string(uri)}\#{query_str}\".freeze
           end
         end
-      Ruby
+      RUBY
     end
 
     def extract_symbols(uri = @uri)
-      uri.split('/').grep(/:.*/).map { |t| t[1..-1].to_sym }
+      uri.split('/').grep(/\A:.+/).map { |t| t[1..-1].to_sym }
     end
 
     def transform_symbols(uri)
@@ -343,23 +367,55 @@ module Shaf
       uri, alt_uri = @uri, @alt_uri
 
       if alt_uri.nil?
-        ->(_ = nil) { uri.freeze }
+        -> { uri }
       else
-        ->(collection = false) { collection ? alt_uri : uri }
+        deprecated_method = template_method_name
+        replacing_method = "#{name}_collection_path_template"
+
+        lambda do |collection = NO_GIVEN_VALUE|
+          if collection != NO_GIVEN_VALUE
+            warn <<~DEPRECATION
+
+              Deprecated use of uri template helper with `collection` argument:
+              Use #{replacing_method} instead of #{deprecated_method}"
+              #{caller.find { |s| !s.match? %r{lib/shaf/extensions/resource_uris.rb} }}
+
+            DEPRECATION
+          else
+            collection = false
+          end
+
+          collection ? alt_uri : uri
+        end
       end
     end
 
     def path_mather_patterns
       [
-        @uri.gsub(%r{:[^/]*}, '\w+'),
-        @alt_uri&.gsub(%r{:[^/]*}, '\w+')
+        uri.gsub(%r{:[^/]*}, '\w+'),
+        alt_uri&.gsub(%r{:[^/]*}, '\w+')
       ].compact.map { |str| Regexp.new("\\A#{str}\\Z") }
     end
 
     def path_matcher_proc
       patterns = path_mather_patterns
 
-      lambda do |path = nil, collection: false|
+      deprecated_method = path_matcher_name
+      replacing_method = "#{name}_collection_path?"
+
+      lambda do |path = nil, collection: NO_GIVEN_VALUE|
+        if collection != NO_GIVEN_VALUE
+          warn <<~DEPRECATION
+
+            Deprecated use of uri predicate helper with `collection` argument:
+            Use #{replacing_method} instead of #{deprecated_method}(collection: true)
+            #{caller.find { |s| !s.match? %r{lib/shaf/extensions/resource_uris.rb} }}
+
+          DEPRECATION
+        else
+          collection = false
+        end
+
         unless path
           r = request if respond_to? :request
           path = r.path_info if r&.respond_to? :path_info
