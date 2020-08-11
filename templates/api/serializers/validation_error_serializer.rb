2f1649216da2cc0e10b39accefecfb97c21a3c49
Sammy Henningsson <sammy.henningsson@gmail.com>
2020-08-11 10:11:51 +0200

    Use shaf error profile from lib instead of gist

--- templates/api/serializers/validation_error_serializer.rb
+++ templates/api/serializers/validation_error_serializer.rb
@@ -1,14 +1,10 @@
 class ValidationErrorSerializer < BaseSerializer
   model Shaf::Errors::ValidationError
-  profile Shaf::Settings.error_profile_name
+  profile 'shaf-error'
 
   attribute :title
   attribute :code
   attribute :fields do
     resource.fields
-  end
-
-  link :profile do
-    Shaf::Settings.error_profile_uri
   end
 end
