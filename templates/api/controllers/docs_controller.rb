76a895ceee10d8b1ddf1f44dafcf2b0d4abde03c
Sammy Henningsson <sammy.henningsson@gmail.com>
2020-08-12 00:27:50 +0200

    Make curie links point to profile with fragment id

--- templates/api/controllers/docs_controller.rb
+++ templates/api/controllers/docs_controller.rb
@@ -1,17 +1,14 @@
 class DocsController < BaseController
 
-  register_uri :doc_curie,      '/doc/:resource/rels/{rel}'
   register_uri :profile,        '/doc/profiles/:name'
+  register_uri :doc_curie,      '/doc/profiles/:name{#rel}'
   register_uri :documentation,  '/doc/:resource'
 
   before_action do
     cache_control(:private, http_cache_max_age: :long)
   end
 
-  get :doc_curie_path do
-    respond_with doc, path: request.path_info, rel: params[:rel]
-  end
-
+  # Note: This route handles both :profile_path and :doc_curie_path
   get :profile_path do
     respond_with Shaf::Profiles.find!(params[:name])
   end
