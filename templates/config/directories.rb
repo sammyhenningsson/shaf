ef2b7b4b168452a30176da05e7a0b18e7df3ed81
Sammy Henningsson <sammy.henningsson@gmail.com>
2020-10-28 02:05:39 +0100

    Wrap directories config inside module

--- templates/config/directories.rb
+++ templates/config/directories.rb
@@ -1,53 +1,61 @@
 require 'shaf/settings'
 
-def sort_files(files)
-  files.sort_by do |file|
-    directory_priority(file) + file_priority(file)
-  end
-end
+module Shaf
+  module Directories
+    class << self
+      def require_ruby_files
+        files = Dir[File.join('**', '*.rb')]
+        sort_files(files).each do |file|
+          # load all files with .rb extension in subfolders of api
+          Shaf.log.debug "Require file: #{file}"
+          require file
+        end
+      end
 
-def directory_priority(file)
-  case file
-  when /\Alib/
-    0
-  when /\Ahelpers/
-    10
-  when /\Amodels/
-    20
-  when /\Apolicies/
-    30
-  when /\Acontrollers/
-    40
-  when /\Aserializers/
-    50
-  else
-    60
-  end
-end
+      private
 
-def file_priority(file)
-  case File.basename(file)
-  when /\Abase[\._]/
-    0
-  else
-    9
-  end
-end
+      def sort_files(files)
+        files.sort_by do |file|
+          directory_priority(file) + file_priority(file)
+        end
+      end
 
-def require_ruby_files
-  files = Dir[File.join('**', '*.rb')]
-  sort_files(files).each do |file|
-    # load all files with .rb extension in subfolders of api
-    Shaf.log.debug "Require file: #{file}"
-    require file
-  end
-end
+      def directory_priority(file)
+        case file
+        when /\Alib/
+          0
+        when /\Ahelpers/
+          10
+        when /\Amodels/
+          20
+        when /\Apolicies/
+          30
+        when /\Acontrollers/
+          40
+        when /\Aserializers/
+          50
+        else
+          60
+        end
+      end
 
-%i[lib_dir src_dir app_dir].each do |cfg|
-  dir = Shaf::Settings.send cfg
-  next unless Dir.exist? dir
+      def file_priority(file)
+        case File.basename(file)
+        when /\Abase[\._]/
+          0
+        else
+          9
+        end
+      end
+    end
 
-  Dir.chdir(dir) do
-    require_ruby_files
+    %i[lib_dir src_dir app_dir].each do |cfg|
+      dir = Shaf::Settings.send cfg
+      next unless Dir.exist? dir
+
+      Dir.chdir(dir) do
+        require_ruby_files
+      end
+    end
   end
 end
