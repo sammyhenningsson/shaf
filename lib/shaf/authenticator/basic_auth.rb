263fc2387a5d2784e9de93b38a09458d7d123f6a
Sammy Henningsson <sammy.henningsson@gmail.com>
2021-02-10 23:37:20 +0100

    Set user and password to nil if empty strings

--- lib/shaf/authenticator/basic_auth.rb
+++ lib/shaf/authenticator/basic_auth.rb
@@ -6,13 +6,14 @@ module Shaf
       param :realm
       param :charset, required: false, values: ["UTF-8"]
 
-      def self.credentials(authorization, _)
+      def self.credentials(authorization, _request)
         return unless authorization
 
         decoded = String(authorization.unpack("m*").first)
         return {} if decoded.empty?
 
         user, password = decoded.split(/:/, 2)
+                                .map { |str| str unless String(str).empty? }
 
         {
           user: user,
