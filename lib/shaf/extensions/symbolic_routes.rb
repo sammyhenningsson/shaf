493ac7a802012f4ffdfb7df5ddf4e45777d8ea62
Sammy Henningsson <sammy.henningsson@gmail.com>
2021-10-03 11:07:14 +0200

    Remove suport for route with collection bool
    Declaring routes with collection keyword arg has been deprecated and is
    support for it is now dropped.
    
    Instead of doing:
    get :fruit_path, collection: true do
      respond_with Fruit.all
    end
    
    We must now instead write:
    get :fruit_path_collection do
      respond_with Fruit.all
    end

--- lib/shaf/extensions/symbolic_routes.rb
+++ lib/shaf/extensions/symbolic_routes.rb
@@ -4,30 +4,17 @@ module Shaf
 
     Shaf::SUPPORTED_HTTP_METHODS.each do |m|
       define_method m do |path, **options, &block|
-        collection = options.delete(:collection)
-        path = rewrite_path(path, collection, m)
+        path = rewrite_path(path, m)
         super(path, **options, &block)
       end
     end
 
-    def rewrite_path(path, collection, method)
+    def rewrite_path(path, method)
       return path unless path.is_a? Symbol
 
-      warn <<~DEPRECATION unless collection.nil?
-        Deprecated use of declaring route with collection keyword argument:
-        Use `#{method} :#{path.to_s.sub(/_(path|uri)/, '_collection_path')} do`
-        instead of `#{method} :#{path}, collection: #{collection} do`
-        #{caller.find { |s| s.match?(/_controller.rb/) }}
-
-      DEPRECATION
-
-      method = "#{path}_template"
-      send_args = [method]
-      send_args << collection unless collection.nil?
-      return send(*send_args) if respond_to? method
-
-      method = "#{path}_path_template"
-      return send(*send_args) if respond_to? method
+      ["#{path}_template", "#{path}_path_template"].each do |method|
+        return send(method) if respond_to? method
+      end
 
       raise UriHelperNotRegisterdError, <<~RUBY
         Undefined method '#{method}'. Did you forget to register a uri helper for #{path}?
